7f04bba7eca64f3910506a616cc61e70b2a11052
Kyoungsik Choi <38401389+kyoungsik-dev@users.noreply.github.com>
2023-01-11 14:38:21 +0900

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -6,13 +6,13 @@ echo "########################################"
 echo ""
 echo "1. Docker 빌드를 시작합니다."
 echo ""
-docker build --tag kyoungsikdev/sk-cafeteria:latest .
+docker build --tag kyoungsikdev/cloud-ping:latest .
 echo ""
 echo "Docker 빌드 완료"
 echo ""
 echo "2. Docker Hub 푸시를 시작합니다."
 echo ""
-docker push kyoungsikdev/sk-cafeteria:latest
+docker push kyoungsikdev/cloud-ping:latest
 echo ""
 echo "푸시 완료"
 echo ""
